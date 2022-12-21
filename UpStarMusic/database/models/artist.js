c53d43c807ada84dc0be9b757804d47e18a7406b
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2022-12-21 14:39:26 +0600

    again added UpStarMusic after deleting

---
+++ UpStarMusic/database/models/artist.js
@@ -0,0 +1,16 @@
+// Todo: Create Artist Model
+const mongoose = require("mongoose");
+const AlbumSchema = require("./album");
+const ArtistSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    yearsActive: Number,
+    image: String,
+    genre: String,
+    website: String,
+    netWorth: Number,
+    labelName: String,
+    retired: Boolean,
+    albums: [AlbumSchema],
+});
+module.exports = mongoose.model("artist", ArtistSchema);
